9ff31ba8a94cabec316aa368a9b8633f8dffd931
AbdullahSaied52 <a9407602@gmail.com>
2026-07-24 15:36:44 +0300

    add rest files for this task

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDTO;
+using process_layer;
+
+namespace FAWRY_assigment
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ClsDTO car = new ClsDTO();
+            car.type = "car";
+            car.date =new DateTime();
+            car.speed = 99;
+            car.plate_number = "QWE1477";
+            car.seatbelt_status = true;
+            process.show(car);
+            Console.WriteLine();
+
+            ClsDTO car2 = new ClsDTO();
+            car2.type = "truck";
+            car2.date = new DateTime();
+            car2.speed = 100;
+            car2.plate_number = "QWE1596";
+            car2.seatbelt_status = false;
+            process.show(car2);
+
+        }
+    }
+}
